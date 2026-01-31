2ffc5d35375d7f2b547b9fb2c3a412a8020bbd4a
Suman Dhami <sumandhami86@gmail.com>
2026-02-01 00:30:00 +0545

    implemented coderabbit suggestion

--- src/pages/classes/create.tsx
+++ src/pages/classes/create.tsx
@@ -5,7 +5,7 @@ import {useBack} from "@refinedev/core";
 import {Separator} from "@/components/ui/separator.tsx";
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card.tsx"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+import { useForm} from "@refinedev/react-hook-form";
 import {classSchema} from "@/lib/schema.ts";
 import * as z from "zod";
 
@@ -77,7 +77,7 @@ const Create = () => {
 
     const bannerPublicId = form.watch('bannerCldPubId');
 
-    const setBannerImage = (file, field) => {
+    const setBannerImage = (file: any, field: any) => {
         if(file) {
             field.onChange(file.url);
             form.setValue('bannerCldPubId', file.publicId, {
@@ -131,7 +131,7 @@ const Create = () => {
                                                    value={field.value ? { url:
                                                        field.value, publicId:
                                                        bannerPublicId ?? ''} : null}
-                                                       onChange={(file: any, field: any) =>
+                                                       onChange={(file: any) =>
                                                            setBannerImage(file, field)}
                                                />
                                            </FormControl>
