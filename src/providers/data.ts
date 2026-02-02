069a76767a9b80540dc263fd31c1384c04680200
Suman Dhami <sumandhami86@gmail.com>
2026-02-02 13:34:59 +0545

    implement class details page

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,7 +1,7 @@
 import {createDataProvider, CreateDataProviderOptions} from "@refinedev/rest";
 import {BACKEND_BASE_URL} from "@/constants";
 import {CreateResponse, ListResponse} from "@/types";
-import {HttpError} from "@refinedev/core";
+import {GetOneResponse, HttpError} from "@refinedev/core";
 
 if (!BACKEND_BASE_URL) throw new Error("BACKEND_BASE_URL is not configured. Please set VITE_BACKEND_BASE_URL in your .env file.");
 
@@ -76,6 +76,16 @@ const options: CreateDataProviderOptions = {
 
         mapResponse: async (response) => {
             const json: CreateResponse = await response.json();
+
+            return json.data ?? [];
+        }
+    },
+
+    getOne: {
+        getEndpoint: ({ resource, id}) => `${resource}/${id}`,
+
+        mapResponse: async (response) => {
+            const json: GetOneResponse = await response.json();
 
             return json.data ?? [];
         }
